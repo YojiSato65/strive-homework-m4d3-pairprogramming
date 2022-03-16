af26af85e75f0a6a543b479015d45262620b523c
Ana Matei <anamatei.099@gmail.com>
2022-03-16 18:33:47 +0000

    Exercise 6&7

--- m4d3/src/components/BookList.jsx
+++ m4d3/src/components/BookList.jsx
@@ -1,37 +1,40 @@
-import {Container, Row, Col, Form} from 'react-bootstrap'
-import SingleBook from './SingleBook';
-import {Component} from 'react'
+import { Container, Row, Col, Form } from "react-bootstrap";
+import SingleBook from "./SingleBook";
+import { Component } from "react";
 
 // const BookList = (props) => {
 class BookList extends Component {
-    state = {
-        searchQuery: ''
-    }
-    render()
-    {
-        return (
-            <Container>
-                <Row>
-                    <Col>
-                    <Form.Group>
-                            <Form.Label>Search</Form.Label>
-                            <Form.Control
-                                type="text"
-                                placeholder="Search here"
-                                value={this.state.searchQuery}
-                                onChange={e => this.setState({ searchQuery: e.target.value })}
-                            />
-                        </Form.Group>
-                    </Col>
-                    </Row>
-                    <Row>
-                    {this.props.books.filter(book => book.title.toLowerCase().includes(this.state.searchQuery)).map(book => (
-                        <SingleBook title={book.title} image={book.img} key={book.asin} />
-                    ))}
-                </Row>
-            </Container>
-        );
-    }
+  state = {
+    searchQuery: "",
+  };
+  render() {
+    return (
+      <Container>
+        <Row>
+          <Col>
+            <Form.Group>
+              <Form.Label>Search</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Search here"
+                value={this.state.searchQuery}
+                onChange={(e) => this.setState({ searchQuery: e.target.value })}
+              />
+            </Form.Group>
+          </Col>
+        </Row>
+        <Row>
+          {this.props.books
+            .filter((book) =>
+              book.title.toLowerCase().includes(this.state.searchQuery)
+            )
+            .map((book) => (
+              <SingleBook title={book.title} image={book.img} key={book.asin} />
+            ))}
+        </Row>
+      </Container>
+    );
+  }
 }
-  
-  export default BookList
+
+export default BookList;
--- m4d3/src/components/SingleBook.jsx
+++ m4d3/src/components/SingleBook.jsx
@@ -1,17 +1,26 @@
 import { Card } from "react-bootstrap";
+import React from 'react'
 
 
-const SingleBook = (props) => {
+class SingleBook extends React.Component {
+
+    state = {
+        selected:false
+    }
+
+render (){
+    return (
+        <Card  onClick={() => this.setState({ selected: !this.state.selected })}
+        style={{ border: this.state.selected ? '5px solid blue' : 'none' }}>
+          <Card.Img variant="top" src={this.props.image} />
+          <Card.Body>
+            <Card.Title>{this.props.title}</Card.Title>
+          </Card.Body>
+        </Card>
+      );
+}
 
 
-  return (
-    <Card style={{ width: "18rem" }}>
-      <Card.Img variant="top" src={props.image} />
-      <Card.Body>
-        <Card.Title>{props.title}</Card.Title>
-      </Card.Body>
-    </Card>
-  );
 };
 
 export default SingleBook
