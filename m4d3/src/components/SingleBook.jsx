7df7801abff080c27f88ee5d20d4d66394799148
YojiSato65 <yojisato65@gmail.com>
2022-03-17 15:44:28 +0200

    solved m4d3hw ex.6-7

--- m4d3/src/components/SingleBook.jsx
+++ m4d3/src/components/SingleBook.jsx
@@ -1,16 +1,29 @@
 import { Card, Col } from 'react-bootstrap'
+import { Component } from 'react'
 
-const SingleBook = (props) => {
-  return (
-    <Col className="col-md-3">
-      <Card>
-        <Card.Img variant="top" src={props.image} />
-        <Card.Body>
-          <Card.Title>{props.title}</Card.Title>
-        </Card.Body>
-      </Card>
-    </Col>
-  )
+class SingleBook extends Component {
+  state = {
+    selectedBook: false,
+  }
+  render() {
+    return (
+      <Col className="col-md-3">
+        <Card
+          onClick={() =>
+            this.setState({ selectedBook: !this.state.selectedBook })
+          }
+          style={{
+            border: this.state.selectedBook ? '2px solid green' : 'none',
+          }}
+        >
+          <Card.Img variant="top" src={this.props.image} />
+          <Card.Body>
+            <Card.Title>{this.props.title}</Card.Title>
+          </Card.Body>
+        </Card>
+      </Col>
+    )
+  }
 }
 
 export default SingleBook
